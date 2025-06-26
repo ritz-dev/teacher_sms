47af12ec8c978716f6a89ee2c7a92581a1291fda
dev <dev@ritz.com.mm>
2025-06-26 15:55:32 +0630

    ADD

--- app/Http/Controllers/APIs/AttendanceController.php
+++ app/Http/Controllers/APIs/AttendanceController.php
@@ -2,8 +2,9 @@
 
 namespace App\Http\Controllers\APIs;
 
-use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use App\Http\Controllers\Controller;
 
 class AttendanceController extends Controller
 {
