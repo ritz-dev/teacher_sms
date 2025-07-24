c2e88a3d44c58581095ce3c80b48936d342b9450
ritz-dev <dev2@ritz.com.mm>
2025-07-24 22:02:20 +0630

    Add

--- app/Http/Controllers/APIs/AssessmentResultController.php
+++ app/Http/Controllers/APIs/AssessmentResultController.php
@@ -47,6 +47,41 @@ class AssessmentResultController extends Controller
 
             $results = $query->get();
 
+            $studentSlugs = collect($results)
+                ->pluck('student_slug')
+                ->filter()
+                ->unique()
+                ->values()
+                ->all();
+                    $studentData = [];
+                    if (!empty($studentSlugs)) {
+                        $baseUrl = config('services.user.url'); // Make sure this is set in config/services.php
+                        $endpoint = $baseUrl . 'students';
+
+                        $response = Http::withHeaders([
+                            'Accept' => 'application/json',
+                        ])->post($endpoint, ['slugs' => $studentSlugs]);
+
+                        if ($response->successful()) {
+                            $studentData = collect($response->json('data'))->keyBy('slug')->toArray();
+                        }
+                    }
+
+                    $results = collect($results)->map(function ($item) use ($studentData) {
+                        $data = (array) $item;
+
+                        if (!empty($item->student_slug) && isset($studentData[$item->student_slug])) {
+                            $student = $studentData[$item->student_slug];
+                            $data['student_name'] = $student['student_name'] ?? null;
+                            $data['student_number'] = $student['student_number'] ?? null;
+                        } else {
+                            $data['student_name'] = null;
+                            $data['student_number'] = null;
+                        }
+
+                        return $data;
+                    });
+
             return response()->json([
                 'success' => true,
                 'total' => $total,
